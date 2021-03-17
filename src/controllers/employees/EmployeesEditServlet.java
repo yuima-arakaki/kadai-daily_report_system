9669d2d78e8940f77dbf76ac0ae3e1b7360ceab3
yuima.kaki <voltzmen176@gmail.com>
2021-03-17 11:39:50 +0900

    Add Employee edit

---
+++ src/controllers/employees/EmployeesEditServlet.java
@@ -0,0 +1,39 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+
+@WebServlet("/employees/edit")
+public class EmployeesEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public EmployeesEditServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("employee", e);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("employee_id",e.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
