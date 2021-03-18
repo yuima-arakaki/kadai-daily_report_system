8cce83dc152c9b52191b501a67f9497192e7ea65
yuima.kaki <voltzmen176@gmail.com>
2021-03-18 12:20:04 +0900

    Add login / logout

---
+++ src/controllers/login/LogoutServlet.java
@@ -0,0 +1,26 @@
+package controllers.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute("login_employee");
+
+        request.getSession().setAttribute("flush", "ログアウトしました。");
+        response.sendRedirect(request.getContextPath() + "/login");
+    }
+
+}
